11fa5a8c915048375c95a69103960cf721b18eb3
limjongwoo <whddn528@naver.com>
2021-08-02 15:01:43 +0900

    Order create,business method add

--- src/main/java/me/jongwoo/jpashop/domain/Order.java
+++ src/main/java/me/jongwoo/jpashop/domain/Order.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -47,5 +49,37 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        Arrays.stream(orderItems).forEach(order::addOrderItem);
+
+        order.setOrderStatus(OrderStatus.ORDER);
+
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 주문 취소
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setOrderStatus(OrderStatus.CANCEL);
+
+        for(OrderItem orderItem : this.orderItems){
+            orderItem.cancel();
+        }
+    }
+    // 조회 로직
+    public int getTotalPrice(){
+        int totalPrice = orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+        return totalPrice;
     }
 }
