382575b3305c567703a5cd321d4b36e4073a10d8
limjongwoo <whddn528@naver.com>
2021-08-11 10:21:42 +0900

    item modify add

--- src/main/java/me/jongwoo/jpashop/controller/BookForm.java
+++ src/main/java/me/jongwoo/jpashop/controller/BookForm.java
@@ -1,17 +1,16 @@
 package me.jongwoo.jpashop.controller;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Data
 public class BookForm {
 
     private Long id;
-
     private String name;
     private int price;
     private int stockQuantity;
-
     private String author;
     private String isbn;
 }
